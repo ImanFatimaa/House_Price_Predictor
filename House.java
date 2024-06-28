d6670d41abd3eb2d5ce966093db050f407cbd52d
ImanFatimaa <168275744+ImanFatimaa@users.noreply.github.com>
2024-06-28 08:27:52 +0500

    Create House.java

---
+++ House.java
@@ -0,0 +1,13 @@
+package codeclause;
+
+public class House {
+    int bedrooms;
+    int squareFootage;
+    double price;
+
+    public House(int bedrooms, int squareFootage, double price) {
+        this.bedrooms = bedrooms;
+        this.squareFootage = squareFootage;
+        this.price = price;
+    }
+}
